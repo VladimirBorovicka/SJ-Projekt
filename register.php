9efeccddb200727cb99909d4b4974505f4739ac7
VladimirBorovicka <162418320+VladimirBorovicka@users.noreply.github.com>
2024-05-25 22:44:16 +0200

    registracia

--- register.php
+++ register.php
@@ -1,155 +1,113 @@
 <?php
-require_once "connectDB.php";
+require_once "include/connectDB.php";
+require_once "include/classes.php";
 
-$username = $password = $confirm_password = $first_name = $surname = $email = "";
-$username_err = $password_err = $confirm_password_err = $first_name_err = $surname_err = $email_err = "";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['firstname']) && isset($_POST['surname']) && isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password'])) {
+        $firstname = $_POST['firstname'];
+        $surname = $_POST['surname'];
+        $username = $_POST['username'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $user = new Users($conn);
+        $result = $user->register($firstname, $surname, $username, $email, $password);
 
-    if(empty(trim($_POST["username"]))){
-        $username_err = "Please enter a username.";
-    } elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["username"]))){
-        $username_err = "Username can only contain letters, numbers, and underscores.";
-    } else{
-        $sql = "SELECT id FROM users WHERE username = ?";
-
-        if($stmt = mysqli_prepare($link, $sql)){
-            mysqli_stmt_bind_param($stmt, "s", $param_username);
-
-            $param_username = trim($_POST["username"]);
-
-            if(mysqli_stmt_execute($stmt)){
-                mysqli_stmt_store_result($stmt);
-
-                if(mysqli_stmt_num_rows($stmt) == 1){
-                    $username_err = "This username is already taken.";
-                } else{
-                    $username = trim($_POST["username"]);
-                }
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
-
-            mysqli_stmt_close($stmt);
+        if ($result === false) {
+            echo "Registration failed.";
+        } else {
+            header("Location: login.php");
+            exit;
         }
     }
-
-    if(empty(trim($_POST["password"]))){
-        $password_err = "Please enter a password.";
-    } elseif(strlen(trim($_POST["password"])) < 6){
-        $password_err = "Password must have atleast 6 characters.";
-    } else{
-        $password = trim($_POST["password"]);
+    else {
+        echo "Please fill in all fields.";
     }
-
-    if(empty(trim($_POST["confirm_password"]))){
-        $confirm_password_err = "Please confirm password.";
-    } else{
-        $confirm_password = trim($_POST["confirm_password"]);
-        if(empty($password_err) && ($password != $confirm_password)){
-            $confirm_password_err = "Password did not match.";
-        }
-    }
-
-    if(empty(trim($_POST["first_name"]))){
-        $first_name_err = "Please enter your first name.";
-    } else{
-        $first_name = trim($_POST["first_name"]);
-    }
-
-    if(empty(trim($_POST["surname"]))){
-        $surname_err = "Please enter your surname.";
-    } else{
-        $surname = trim($_POST["surname"]);
-    }
-
-    if(empty(trim($_POST["email"]))){
-        $email_err = "Please enter your email address.";
-    } elseif(!filter_var(trim($_POST["email"]), FILTER_VALIDATE_EMAIL)){
-        $email_err = "Invalid email address format.";
-    } else{
-        $email = trim($_POST["email"]);
-    }
-
-    if(empty($username_err) && empty($password_err) && empty($confirm_password_err) && empty($first_name_err) && empty($surname_err) && empty($email_err)){
-
-        $sql = "INSERT INTO users (username, password, first_name, surname, email) VALUES (?, ?, ?, ?, ?)";
-
-        if($stmt = mysqli_prepare($link, $sql)){
-            mysqli_stmt_bind_param($stmt, "sssss", $param_username, $param_password, $param_first_name, $param_surname, $param_email);
-
-            $param_username = $username;
-            $param_password = $password;
-            $param_first_name = $first_name;
-            $param_surname = $surname;
-            $param_email = $email;
-
-            if(mysqli_stmt_execute($stmt)){
-                header("location: login.php");
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
-
-            mysqli_stmt_close($stmt);
-        }
-    }
-
-    mysqli_close($link);
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Sign Up</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <style>
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 360px; padding: 20px; }
-    </style>
+    <meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		 <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+		<title>Electro - HTML Ecommerce Template</title>
+
+		<!-- Google font -->
+		<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
+
+		<!-- Bootstrap -->
+		<link type="text/css" rel="stylesheet" href="css/bootstrap.min.css"/>
+
+		<!-- Slick -->
+		<link type="text/css" rel="stylesheet" href="css/slick.css"/>
+		<link type="text/css" rel="stylesheet" href="css/slick-theme.css"/>
+
+		<!-- nouislider -->
+		<link type="text/css" rel="stylesheet" href="css/nouislider.min.css"/>
+
+		<!-- Font Awesome Icon -->
+		<link rel="stylesheet" href="css/font-awesome.min.css">
+
+		<!-- Custom stlylesheet -->
+		<link type="text/css" rel="stylesheet" href="css/style.css"/>
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
 </head>
+
 <body>
-    <div class="wrapper">
-        <h2>Sign Up</h2>
-        <p>Please fill this form to create an account.</p>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-group">
-                <label>Username</label>
-                <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
-                <span class="invalid-feedback"><?php echo $username_err; ?></span>
-            </div>    
-            <div class="form-group">
-                <label>Password</label>
-                <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
-                <span class="invalid-feedback"><?php echo $password_err; ?></span>
-            </div>
-            <div class="form-group">
-                <label>Confirm Password</label>
-                <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
-                <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
-            </div>
-            <div class="form-group">
-                <label>First Name</label>
-                <input type="text" name="first_name" class="form-control <?php echo (!empty($first_name_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $first_name; ?>">
-                <span class="invalid-feedback"><?php echo $first_name_err; ?></span>
-            </div>
-            <div class="form-group">
-                <label>Surname</label>
-                <input type="text" name="surname" class="form-control <?php echo (!empty($surname_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $surname; ?>">
-                <span class="invalid-feedback"><?php echo $surname_err; ?></span>
-            </div>
-            <div class="form-group">
-                <label>Email</label>
-                <input type="email" name="email" class="form-control <?php echo (!empty($email_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $email; ?>">
-                <span class="invalid-feedback"><?php echo $email_err; ?></span>
-            </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Submit">
-                <input type="reset" class="btn btn-secondary ml-2" value="Reset">
-            </div>
-            <p>Already have an account? <a href="login.php">Login here</a>.</p>
-        </form>
-    </div>    
+    <?php 
+    include "include/header.php";
+    ?>
+    
+    <div class="container-login">
+        <div class="wrapper">
+            <h2>Register</h2>
+            <form action="" method="post">
+                <div class="form-group">
+                    <label>Username</label>
+                    <input type="text" name="username" class="form-control" value="">
+                    <span class="invalid-feedback"></span>
+                </div>
+                <div class="form-group">
+                    <label>First Name</label>
+                    <input type="text" name="firstname" class="form-control" value="">
+                    <span class="invalid-feedback"></span>
+                </div>
+                <div class="form-group">
+                    <label>Surname</label>
+                    <input type="text" name="surname" class="form-control" value="">
+                    <span class="invalid-feedback"></span>
+                </div>
+                <div class="form-group">
+                    <label>Email</label>
+                    <input type="email" name="email" class="form-control">
+                    <span class="invalid-feedback"></span>
+                </div>
+                <div class="form-group">
+                    <label>Password</label>
+                    <input type="password" name="password" class="form-control">
+                    <span class="invalid-feedback"></span>
+                </div>
+                <div class="form-group">
+                    <input type="submit" class="btn btn-primary" value="Register" name="submit">
+                </div>
+                <p>Already have an account? <a href="login.php">Login here</a>.</p>
+            </form>
+        </div>
+    </div>
+
+    <?php
+    include "include/footer.php";
+    ?>
 </body>
 </html>
