7a10726f53bf98d1ec7c61ffb6f6b18930ad01de
VladimirBorovicka <162418320+VladimirBorovicka@users.noreply.github.com>
2024-06-01 20:57:33 +0200

    autorizacia

--- remove_from_cart.php
+++ remove_from_cart.php
@@ -1,9 +1,9 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 require_once 'include/classes/Cart.php';
 session_start();
+if (!isset($_SESSION['username'])) {
+    header('Location: index.php');
+}
 
 if (isset($_GET['index'])) {
     $item = $_GET['index'];
